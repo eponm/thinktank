208cc705cf731739dd852e3d957591cc5b70450e
eponm <etiologicality@gmail.com>
2015-04-24 10:59:36 -0400

    Added some more preliminary things.

---
+++ IdeaHeap.java
@@ -0,0 +1,11 @@
+/* IdeaHeap.java
+
+    IdeaHeap inherits generic Heap characteristics and extends it for use in thinktank.
+    Nobody is totally sure how necessary this is.
+
+*/
+
+class IdeaHeap extends Heap {
+
+
+} // Idea
