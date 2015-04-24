f0109bc5483a4c996a9873712fd4c1967fdb0387
eponm <etiologicality@gmail.com>
2015-04-24 15:48:24 -0400

    Began filling out some instantiation

--- Student.java
+++ Student.java
@@ -1,12 +1,12 @@
 /* Student.java
 
     Each student includes:
-        A last name
-        Username
-        4-digit SSN
-        4-digit ID
-        10 most recent ideas
-        Average rating of 10 most recent ideas
+        (Input) Last name (of type StringBuffer)
+        (Input) Username (of type StringBuffer)
+        (Input) 4-digit SSN (of type int)
+        (Input) 4-digit ID (of type int)
+        10 most recent ideas (a Queue object)
+        Average rating of 10 most recent ideas (of type float)
 
     All of the above are simple data figures, except the 10 most recent ideas.
     The 10 most recent ideas will be a queue of pointers to the ideas stored in an Ideaheap.
@@ -15,6 +15,38 @@
 */
 
 class Student {
+
+    StringBuffer surname;
+    StringBuffer username;
+    int socSecNum; // Must be 0001–9999
+    int studentID; // Must be 0001–9999
+    //Queue ideaQueue; // This may need to be its own object.
+    float avgIdeaRating;
+
+    // Constructor
+    // Params: Last name, short username, 4-digit SSN, 4-digit student number.
+    public Student(StringBuffer surnameIn, StringBuffer usernameIn, int socSecNumIn, int studentIDIn) {
+
+        // Place all of the input data into instance variables.
+        surname = surnameIn;
+        username = usernameIn; // Checks for 4-digit-ness should be done beforehand
+        socSecNum = socSecNumIn;
+        studentID = studentIDIn;
+
+    } // Constructor
+
+
+//  REQUIRED METHODS:
+    //displayStudent()
+        // to display student information as described in specifications
+    //displayIdeas()
+        // to display 10 recent ideas
+    //deleteStudent()
+        // to delete record (unsure if should delete ideas as well)
+    //editNames()
+        // to change last name and username
+
+
 
 
 } // Student
