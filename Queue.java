68d50bc56c6ee3591c6a8cd95d9c009f955e93fe
eponm <etiologicality@gmail.com>
2015-04-27 22:40:46 -0400

    Made some progress! A little bit.

--- Queue.java
+++ Queue.java
@@ -1,28 +1,38 @@
 import java.util.Scanner;
+
 public class Queue {
+
     int front=0;
     int end=0;
     int n=0;
     Idea[] queue= new Idea[10];
-    
+
+
     public void enqueue(Idea x) {
         queue[end]=x;
         end=(end+1) % 10;
-    }
+    } // constructor
+
 
     public void printQueue() {
         System.out.println(front);
         System.out.println(end);
-        if (front <= end)
-           for(int i = front; i < end; i++) 
+        if (front <= end) {
+           for(int i = front; i < end; i++) {
                System.out.println(queue[i].getKey());
+           } // for
+        } // if
         else {
-           for(int i = front; i < 10; i++) 
-               System.out.println(queue[i].getKey());          
-           for(int i = 0; i < end; i++) 
+           for(int i = front; i < 10; i++) {
                System.out.println(queue[i].getKey());
-        }          
-    }
+           } // for
+           for(int i = 0; i < end; i++) {
+               System.out.println(queue[i].getKey());
+           } // for
+        } // else
+    } // printQueue
+
+
     public boolean isEmpty() {
         if (front==end) {
             return true;
@@ -30,7 +40,8 @@ public class Queue {
         else {
             return false;
         }
-    }
+    } // isEmpty
+
 
     public Idea front() {
         if (isEmpty() ) {
@@ -39,11 +50,14 @@ public class Queue {
         else {
             return queue[front];
         }
-    }
+    } // front
+
 
     public Idea dequeue() {
         Idea temp=queue[front];
         front=(front+1)% 10;
         return temp;
-    }
-}
+    } // dequeue
+
+
+} // class
