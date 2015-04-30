e9338e37da2510cecc84bc9d208b246615c015f9
eponm <etiologicality@gmail.com>
2015-04-30 15:08:56 -0400

    Began some abstract descriptions

---
+++ ThinkTank.java
@@ -0,0 +1,21 @@
+class ThinkTank {
+
+    public static void main(String[] args) {
+
+        // First: Check for a config file.
+        // If one exists, load it into memory
+        // Else, write a default config
+
+        // All of the following is read out of the config file:
+
+        // Init the student tree
+        // Init the idea heap
+
+        // Load the students into the tree in a manner that is not in order
+        // Load the ideas into the heap
+
+
+
+    } // MAIN
+
+} // Thinktank
