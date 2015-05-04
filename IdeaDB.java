2f3f6b973be705871e6fc702d2ec08d955121da4
eponm <etiologicality@gmail.com>
2015-05-04 13:32:23 -0400

    Started to outline methods

--- IdeaDB.java
+++ IdeaDB.java
@@ -1,5 +1,31 @@
+import Heap;
+import List;
+
 class IdeaDB {
 
+    private List coreList();
+    private Heap ideaHeap();
+
+    private int seqNum;
+    private
+
+    // Constructor
+    public IdeaDB() {
+
+    } // constructor
 
 
-}
+    // Insert - adds a new idea
+    public void insert(Idea newIdea) {
+
+    } // insert
+
+
+    // "Sell" - deletes top-rated idea
+    public void sell() {
+
+    } // sell
+
+
+
+} // class
