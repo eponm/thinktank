8bef15382a3e50a5c4515e253209b68bcce80495
eponm <etiologicality@gmail.com>
2015-05-04 14:36:05 -0400

    Added flip() and isInHeap()

--- Heap.java
+++ Heap.java
@@ -11,8 +11,14 @@
 //printHeap()
 
 public class Heap {
-    int n=0;
-    Idea[] heap=new Idea[128];
+    int n;
+    Idea[] heap;
+
+    // Constructor
+    public Heap() {
+      n = 0;
+      heap = new Idea[128];
+    } // constructor
 
     public boolean isEmptyHeap() {
         return n==0;
