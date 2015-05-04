a00869ac5aff9f2b6cb96b019740ec591f02d17d
eponm <etiologicality@gmail.com>
2015-05-04 16:23:49 -0400

    Not sure what changed but I saved so I'm commiting it

--- IdeaDB.java
+++ IdeaDB.java
@@ -2,6 +2,7 @@ class IdeaDB {
 
     private List coreList;
     private Heap ideaHeap;
+    private BinaryTree studentTree;
 
     private int seqNum;
 
@@ -10,6 +11,7 @@ class IdeaDB {
         seqNum = 1;
         coreList = new List();
         ideaHeap = new Heap();
+        studentTree = new BinaryTree();
     } // constructor
 
 
@@ -24,12 +26,17 @@ class IdeaDB {
         ideaHeap.insert(newIdea);
         // Flip idea's inHeap value
         newIdea.inHeap(true);
+
+        // Access student from BST
+        // Add idea to student's queue
+
     } // insert
 
 
-    // "Sell" - deletes top-rated idea
-    public void sell() {
-        return; //debug
+    // "Sell" - deletes top-rated idea and returns it
+    public Idea sell() {
+        // Store the min in a temporary slot and get rid of it from the heap
+        temp = ideaHeap.deleteMin();
     } // sell
 
 
