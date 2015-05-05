4ea3be2b7351b2407a300b582f77ea412ac8c0e7
jessica-spencer <jspence4@conncoll.edu>
2015-05-05 14:59:06 -0400

    this is a half finished heap-- I'm working on it

--- Heap.java
+++ Heap.java
@@ -1,57 +1,61 @@
-//Jessica Spencer
-//Heap 
-//  array of nodes (you can assume a max size of 120 or so). Write your own test program that tests each of the heap functions. Make sure that the test program handles all of the possibilities; you will be graded on your test program as well as your class definition. This assignment is due 14 Apr at the start of class. Submit your class description for Heap and Node, and you test program.
-
 //heap -- min number is next number helped
-//heap functions:
-//isEmptyHeap()
-//findMin();
-//deleteMin();
-//insert(Node x);
-//printHeap()
-
 public class Heap {
-    int n=0;
-    Node[] heap=new Node[120];
+    int n; //counter for 
+    Node[] heap;//array of nodes
     
+    //constructor
+    //params: n, node[]
+    public class(int n, Node[] heap) {
+        int n=0;
+        heap=new Node[128];
+    }//constructor
+    
+//isEmptyHeap() checks if heap is empty    
     public boolean isEmptyHeap() {
         return n==0;
     }
     
-    public int findMin() {
+//findMin();finds and returns the only accessible value in the heap
+    public String findMin() {
         if (isEmptyHeap()==true) {
-            return 0;
+            //return 0;
+            System.out.println("There are no ideas here");//debug
         }
-        return heap[0].getKey();
-    }
+        else {
+            System.out.println("The best idea is Idea #" + heap[0].getSeqNum() + "/nrated at" + heap[0].getRating() + "here is the description: /n" + heap.getDesc());//debug
+        }
+    }  
     
-    
+ //printHeap() prints all the values in the heap in order  
+ // debug
     public void printHeap() {
         for (int i=0;i<(n);i++) {
             System.out.println(heap[i].getKey());
         }
-    }
+    } 
     
-    
+ //insert(Node x); inserts node into heap reorders heap   
     public void insert(Node x) {
         heap[n]=x;
         if (n!=0) {
             swap(n-1);
         }
         n+=1;
-    }
-        
-    //helper function for inserted nodes
+    } 
+    
+//helper function for inserted nodes
      public void swap(int index) {
          int parentIndex=index/2;
+         //if parent is the root
          if (index==2) {
              if (heap[0].getKey()> heap[index+1].getKey()) {
                   Node a=heap[0];
                   Node b=heap[index+1];
                   heap[index+1]=a;
                   heap[0]=b;
-             }
-         }
+             }//swaps parent/child
+         }//if parent is the root
+         //for all other swaps, finds parent with direct index
          while (heap[parentIndex].getKey() > heap[index+1].getKey()){
             Node b=heap[index+1];
             heap[index+1]=heap[parentIndex];
@@ -60,19 +64,22 @@ public class Heap {
          }
      }
     
-    //helper function for rearranging after nodes are deleted
+//helper function for rearranging after nodes are deleted
     public void swap2(int m) {
-         while (heap[m].getKey() > heap[m+1].getKey()){
-            Node b=heap[m+1];
-            Node a=heap[m];
-            heap[m+1]=null;
-            heap[m]=null;
-            heap[m+1]=a;
-            heap[m]=b;
-            swap(m+1); 
+        int j=0;
+         while (heap[m*2].getKey() > (heap[m*2+1].getKey() || heap[m*2+2].getKey()){
+            Node b=heap[m*2+1];
+            Node a=heap[m*2];
+            heap[m*2+1]=null;
+            heap[m*2]=null;
+            heap[m*2+1]=a;
+            heap[m*2]=b;
+            swap(m*2+1); 
+             //for the other child EDIT
          }
      }
    
+//deleteMin(); deletes the min, reassigns the min, reorders tree
     public Node deleteMin() {
         if (n==0) {
             return null;
@@ -85,7 +92,5 @@ public class Heap {
             swap2(0);
         }
         return heap[0];
-    }
-    
-    
+    }   
 }
