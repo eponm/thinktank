785504bb31a8b3617bf6c31a66aeff3bf5b7f897
jessica-spencer <jspence4@conncoll.edu>
2015-05-05 20:29:31 -0400

    here's the finished heap I promise

--- Heap.java
+++ Heap.java
@@ -5,104 +5,144 @@ public class Heap {
     
     //constructor
     //params: n, node[]
-    public class(int n, Node[] heap) {
-        int n=0;
-        heap=new Node[128];
+    public Heap() {
+        n=0;
+        heap=new Node[256];
     }//constructor
     
 //isEmptyHeap() checks if heap is empty    
     public boolean isEmptyHeap() {
         return n==0;
-    }
+    }//isEmptyHeap
     
 //findMin();finds and returns the only accessible value in the heap
-    public String findMin() {
+    public Node findMin() {
         if (isEmptyHeap()==true) {
-            //return 0;
+            return heap[0];
             System.out.println("There are no ideas here");//debug
-        }
+        }//if
         else {
             System.out.println("The best idea is Idea #" + heap[0].getSeqNum() + "/nrated at" + heap[0].getRating() + "here is the description: /n" + heap.getDesc());//debug
-        }
-    }  
+        }//else
+    }//findMin
     
  //printHeap() prints all the values in the heap in order  
  // debug
     public void printHeap() {
         for (int i=0;i<(n);i++) {
             System.out.println(heap[i].getKey());
-        }
-    } 
+        }//for
+    }//printHeap
     
  //insert(Node x); inserts node into heap reorders heap   
     public void insert(Node x) {
         heap[n]=x;
         if (n!=0) {
             swap(n-1);
-        }
+        }//if
         n+=1;
-    } 
+    }//insert
     
 //helper function for inserted nodes
      public void swap(int index) {
          int parentIndex=index/2;
-         //if parent is the root
-         if (index==2) {
-             if (heap[0].getKey()> heap[index+1].getKey()) {
+         //for when there are 2 or 3 items in the array
+         if (index==1) {
+             //if there are only two items
+             if (heap[0].getKey()> heap[1].getKey()) {
                   Node a=heap[0];
-                  Node b=heap[index+1];
-                  heap[index+1]=a;
+                  Node b=heap[1];
+                  heap[1]=a;
                   heap[0]=b;
-             }//swaps parent/child
-         }//if parent is the root
-         //for all other swaps, finds parent with direct index
-         while (heap[parentIndex].getKey() > heap[index+1].getKey()){
-            Node b=heap[index+1];
-            heap[index+1]=heap[parentIndex];
-            heap[parentIndex]=b;
-            swap(parentIndex); 
-         }
-     }
+                  return;
+             }//if
+             //if there are 3 items
+             else if(heap[2]!=null) {
+                 if (heap[0].getKey()> heap[2].getKey()) {
+                     Node a= heap[0];
+                     Node b=heap[2];
+                     heap[2]=a;
+                     heap[0]=b;
+                 }//if
+             }//else if 
+             else  {
+                 return;
+             }//else
+             
+         }//if (index==1)
+         
+        //if there are 3 things in the array
+        else if (index==2) {
+            if (heap[0].getKey()> heap[2].getKey()) {
+                      Node a=heap[0];
+                      Node b=heap[2];
+                      heap[2]=a;
+                      heap[0]=b;
+                      return;
+                 }//if
+        }//else if (index==2)
+         
+        //for all other swaps at all other lengths, finds parent with direct index
+        while (heap[parentIndex].getKey() > heap[index+1].getKey()){
+                Node b=heap[index+1];
+                heap[index+1]=heap[parentIndex];
+                heap[parentIndex]=b;
+                swap(parentIndex);  
+         }//while
+        }//swap
     
 //helper function for rearranging after nodes are deleted
     public void swap2(int m) {
         int j=0;
-         while (heap[m*2].getKey() > ((heap[m*2+1].getKey() || heap[m*2+2].getKey())){
-            if (heap[m*2].getKey() > heap[m*2+1].getKey()){
-                Node b=heap[m*2+1];
-                Node a=heap[m*2];
-                heap[m*2+1]=null;
-                heap[m*2]=null;
-                heap[m*2+1]=a;
-                heap[m*2]=b;
-                swap(m*2+1); 
-            }
-            else {
-                Node b=heap[m*2+2];
-                Node a=heap[m*2];
-                heap[m*2+2]=null;
-                heap[m*2]=null;
-                heap[m*2+2]=a;
-                heap[m*2]=b;
-                swap(m*2+2); 
-            }
-            
-             //for the other child EDIT
-         }
-     }
+        //if the tree has no child i.e. has nothing to swap with
+        if (heap[m*2+1]==null && heap[m*2+2]==null) {
+            return;
+        }//if
+        //if there is only a left child 
+        else if (heap[m*2+1]==null) {
+            swap(m*2+2);
+        }//else if 
+        //if there is only a right child
+        else if (heap[m*2+2]==null) {
+            swap(m*2+1);
+        }//else if
+        //if there is both children-- evaluates which child has lesser key (smaller key number) and swaps
+        else {
+            while ((heap[m*2].getKey() > heap[m*2+1].getKey()) || (heap[m*2].getKey()> heap[m*2+2].getKey())){
+                if (heap[m*2].getKey() > heap[m*2+1].getKey()){
+                    Node b=heap[m*2+1];
+                    Node a=heap[m*2];
+                    heap[m*2+1]=null;
+                    heap[m*2]=null;
+                    heap[m*2+1]=a;
+                    heap[m*2]=b;
+                    swap(m*2+1); 
+                }//if parent>right child
+                else {
+                    Node b=heap[m*2+2];
+                    Node a=heap[m*2];
+                    heap[m*2+2]=null;
+                    heap[m*2]=null;
+                    heap[m*2+2]=a;
+                    heap[m*2]=b;
+                    swap(m*2+2); 
+                }//else parent>leftchild
+             }//while
+        }//else
+    }
    
 //deleteMin(); deletes the min, reassigns the min, reorders tree
     public Node deleteMin() {
         if (n==0) {
             return null;
-        }   
+        }//if   
         Node newMin=heap[n-1];
         heap[0]=newMin;
         heap[n-1]=null;
         n--;
         if (n>1) {
             swap2(0);
-        }
+        }//if
         return heap[0];
-    }   
-}
+    }//deleteMin()   
+}//public class Heap.java
