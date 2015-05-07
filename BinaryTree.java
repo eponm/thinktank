0f6d8211d9ad71e98aa44f4cc6cd700905a72fd8
jessica-spencer <jspence4@conncoll.edu>
2015-05-07 09:59:23 -0400

    fixed binary tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,7 +2,6 @@ import java.io.PrintWriter; // For writing, obviously, I mean like...DUH!
 
 /*BinaryTree.java
     head - Node type pointer that will point to head
-
     Methods:
         isEmptyTree()
         search()
@@ -84,7 +83,8 @@ public class BinaryTree {
                 return null;
             } // else
         } // else (if no flip)
-
+        //!!!!!!!!!!!!!!!!!!!!!!! weird added in to make compile??
+        return null;
     } // search
 
 
@@ -93,13 +93,6 @@ public class BinaryTree {
     public Student searchTwo(Student branch, int key){
         if (flipKeyToNum = true){
             if (key<branch.getStudentID()){//if key is smaller than branch key
-
-                return searchTwo(branch.getLeft(),key);
-            }//if
-            else if (key>branch.getStudentID()){//if key is smaller than branch key
-                return searchTwo(branch.getRight(),key);
-            }//else if
-
                 if (branch.getLeft() != null) {
                     return searchTwo(branch.getLeft(),key);
                 }
@@ -109,92 +102,56 @@ public class BinaryTree {
                     return searchTwo(branch.getRight(),key);
                 }
             }
-
             else if (branch.getStudentID() == key) { //if key equals branch key
                 return branch;
-            }//else if
+            }
             else {
                 return null;
-            }//else
-        }//if flipKeyToNum
+            }
+        }
         else{
             if (key<branch.getKey()){//if key is smaller than branch key
                 return searchTwo(branch.getLeft(),key);
-            }//if
+            }
             else if (key>branch.getKey()){//if key is smaller than branch key
                 return searchTwo(branch.getRight(),key);
-            }//else if
+            }
             else if (branch.getKey() == key) { //if key equals branch key
                 return branch;
-            }//else if
+            }
             else {
                 return null;
-            }//else
-        }//else
+            }
+        }
+        //!!!!!!!!!!!!!!!!!! weird added in to make compile??
+        return null;
     } // search
 
 
     // insert
     // inserts Student into BST
     // Params: Student to be inserted
-    public void insert(Student newStudent){
-        if (isEmptyTree)) { // if there is no head
+    public void insert(Student newStudent) {
+        if (isEmptyTree()) { // if there is no head
             head=newStudent;
-        }//if
+        }
         else { // if there is a head
-            insertTwo(head,newStudent);
-        }//else
-    }//insert(student)
+            insert2(head,newStudent);
+        }
+    }
 
 
     //insert helper
     //Params: Student in tree, Student to be inserted
-    private void insertTwo(Student parent, Student newStudent){
+    private void insert2(Student parent, Student nodeIn){
         if (flipKeyToNum == true){
-
-            if(newStudent.getStudentID()<branch.getStudentID()){//if Student is smaller branch
-                if(branch.getLeft()==null){//if the branch left child is null
-                    branch.setLeft(newStudent);
-                }//if
-                else{//recursive call for next left branch down
-                    insertTwo(branch.getLeft(),newStudent);
-                }//else
-            }//if flipKeyToNum
-            else{//if newStudent is larger than branch
-                if(branch.getRight()==null){//if the branch right child is null
-                    branch.setRight(newStudent);
-                }//else
-                else{//recursive call or next right branch down
-                    insertTwo(branch.getRight(),newStudent);
-                }//else
-            }//else if newStud is larger
-        }//if flipKeyToNum==true
-        else{
-            if(newStudent.getKey()<branch.getKey()){//if Student is smaller branch
-                if(branch.getLeft()==null){//if the branch left child is null
-                    branch.setLeft(newStudent);
-                }//if
-                else{//recursive call for next left branch down
-                    insertTwo(branch.getLeft(),newStudent);
-                }//else
-            }//if key<branch key
-            else{//if newStudent is larger than branch
-                if(branch.getRight()==null){//if the branch right child is null
-                    branch.setRight(newStudent);
-                }//if
-                else{//recursive call or next right branch down
-                    insertTwo(branch.getRight(),newStudent);
-                }//else
-            }//else
-        }//else
-
             if (nodeIn.getStudentID() > parent.getKey()) {
             if (parent.getRight() != null) {
                 insert2 (parent.getRight(), nodeIn);
             } // if
             else {
                 parent.setRight(nodeIn);
-                nodeIn.setParent(parent);
+                //nodeIn.setParent(parent);
             } // else
         } // if
         else {
@@ -203,10 +160,12 @@ public class BinaryTree {
             } // if
             else {
                 parent.setLeft(nodeIn);
-                nodeIn.setParent(parent);
+               // nodeIn.setParent(parent);
             } // else
         } // else
         }
+        
+        //if flipKeyToNum == false
         else{
             if (nodeIn.getKey() > parent.getKey()) {
                 if (parent.getRight() != null) {
@@ -214,7 +173,7 @@ public class BinaryTree {
                 } // if
                 else {
                     parent.setRight(nodeIn);
-                    nodeIn.setParent(parent);
+                 //   nodeIn.setParent(parent);
                 } // else
             } // if
             else {
@@ -223,11 +182,10 @@ public class BinaryTree {
                 } // if
                 else {
                     parent.setLeft(nodeIn);
-                    nodeIn.setParent(parent);
+                  //  nodeIn.setParent(parent);
                 } // else
             } // else
         }
-
     }//insert
 
 
@@ -238,8 +196,8 @@ public class BinaryTree {
             traverseTwo(head.getLeft());
             System.out.print(head.getKey()+" ");
             traverseTwo(head.getRight());
-        }//if
-    }//transverse
+        }
+    }
 
     //traverse helper
     private void traverseTwo(Student branch){
@@ -247,7 +205,7 @@ public class BinaryTree {
             traverseTwo(branch.getLeft());
             System.out.print(branch.getKey()+" ");
             traverseTwo(branch.getRight());
-        }//if 
+        }
     }//traverse
 
 
@@ -267,18 +225,18 @@ public class BinaryTree {
             if(key<head.getStudentID()){//if Student key is larger than head
                 int side=0;
                 deleteTwo(head.getLeft(),key,head,side);
-            }//if key>head
+            }
             else if(key>head.getStudentID()){//if Student key is smaller than head
                 int side=1;
                 deleteTwo(head.getRight(),key,head,side);
-            }//if key<head
+            }
             else{//if Student key equals head
                 /*
                 pricipal of prioritizing higher values illustrated in this case:
                 */
                 if(head.getLeft()==null&&head.getRight()==null){//if head has no children
                     head=null;
-                }//if head ! have children
+                }
                 else if(head.getLeft()==null){// if head has a right child
                     Student temp=head;
                     Student temp2=popMax(head.getRight(),head,0); //note how it only gets the maximum leftmost
@@ -287,7 +245,7 @@ public class BinaryTree {
                     head=temp2;
                     head.setRight(temp.getLeft());
                     temp.setRight(null);
-                }//ELSE if head has right child
+                }
                 else{// if head has two children
                     Student temp=head;
                     Student temp2=popMax(head.getLeft(),head,1);
@@ -296,26 +254,26 @@ public class BinaryTree {
                     head.setLeft(temp.getLeft());
                     head.setRight(temp.getRight());
                     temp.setLeft(temp.getLeft());
-                }//else if head has two children
-            }//if Student key equals head
-        }//else if flipKeyToNum==true
+                }
+            }
+        }
         else{
             int key = head.getKey();
             if(key<head.getKey()){//if Student key is larger than head
                 int side=0;
                 deleteTwo(head.getLeft(),key,head,side);
-            }//if key>head
+            }
             else if(key>head.getKey()){//if Student key is smaller than head
                 int side=1;
                 deleteTwo(head.getRight(),key,head,side);
-            }//if key<head
+            }
             else{//if Student key equals head
                 /*
                 pricipal of prioritizing higher values illustrated in this case:
                 */
                 if(head.getLeft()==null&&head.getRight()==null){//if head has no children
                     head=null;
-                }//if head has no children
+                }
                 else if(head.getLeft()==null){// if head has a right child
                     Student temp=head;
                     Student temp2=popMax(head.getRight(),head,0); //note how it only gets the maximum leftmost
@@ -324,7 +282,7 @@ public class BinaryTree {
                     head=temp2;
                     head.setRight(temp.getLeft());
                     temp.setRight(null);
-                }//else if head has right child
+                }
                 else{// if head has two children
                     Student temp=head;
                     Student temp2=popMax(head.getLeft(),head,1);
@@ -333,10 +291,10 @@ public class BinaryTree {
                     head.setLeft(temp.getLeft());
                     head.setRight(temp.getRight());
                     temp.setLeft(temp.getLeft());
-                }//else head has 2 children
-            }//else if student equals head
-        }//else
-    }//delete(Student target)
+                }
+            }
+        }
+    }
 
 
     //delete helper
@@ -349,123 +307,123 @@ public class BinaryTree {
             if(key<target.getStudentID()){//if Student key is smaller than target key
                 side=0;
                 deleteTwo(target.getLeft(),key,target,side);
-            }//if key<target
+            }
             else if(key>target.getStudentID()){//if Student key is larger than target key
                 side=1;
                 deleteTwo(target.getRight(),key,target,side);
-            }//if key>target
+            }
             else{//if Student key equals target key
                 if(target.getLeft()==null&&target.getRight()==null){//if the target has no children
                     if(side==0){//if the target is on the left side of parent
                         parent.setLeft(null);
-                    }//if target on left
+                    }
                     else{//if the target is on the right side of parent
                         parent.setRight(null);
-                    }//if target on right
-                }//if target has no children
+                    }
+                }
                 else if(target.getLeft()==null||target.getRight()==null){//if the target has either a right or left child
                     if(side==0){//if target is on the left side of parent
                         if(target.getLeft()==null){//if the target has a right child
                             Student successor=popMax(target.getRight(),parent,0);
                             parent.setLeft(successor);
                             successor.setRight(target.getRight());
-                        }//if target has right child
+                        }
                         else{//if target has a left child
                             Student successor=popMax(target.getLeft(),parent,1);
                             parent.setLeft(successor);
                             successor.setLeft(target.getLeft());
-                        }//if target has left child
-                    }//else if target has either right or left child
+                        }
+                    }
                     else{//if target is on the right side of the parent
                         if(target.getLeft()==null){//if target has a left child
                             Student successor=popMax(target.getRight(),parent,0);
                             parent.setRight(successor);
                             successor.setRight(target.getRight());
-                        }//if target has left child
+                        }
                         else{//if target has a right child
                             Student successor=popMax(target.getLeft(),parent,1);
                             parent.setRight(successor);
                             successor.setLeft(target.getLeft());
-                        }//if target has right child
-                    }//if target on right side of parent
-                }//if target has either right or left child
+                        }
+                    }
+                }
                 else{//if the target has two children
                     if(side==0){//if the target is on the left side of the parent
                         Student successor=popMax(target.getLeft(),parent,1);
                         parent.setLeft(successor);
                         successor.setRight(target.getRight());
                         successor.setLeft(target.getLeft());
-                    }//if target on left side of parent
+                    }
                     else{//if the target is on the right side of the parent
                         Student successor=popMax(target.getLeft(),parent,1);
                         parent.setRight(successor);
                         successor.setRight(target.getRight());
                         successor.setLeft(target.getLeft());
-                    }//ielse if target on right side of parent
-                }//else if target has two children
-            }//if student key == target key
-        }//if flipKeyToNum==true
+                    }
+                }
+            }
+        }
         else{
             if(key<target.getKey()){//if Student key is smaller than target key
                 side=0;
                 deleteTwo(target.getLeft(),key,target,side);
-            }//if student key<target
+            }
             else if(key>target.getKey()){//if Student key is larger than target key
                 side=1;
                 deleteTwo(target.getRight(),key,target,side);
-            }//if student key>target
+            }
             else{//if Student key equals target key
                 if(target.getLeft()==null&&target.getRight()==null){//if the target has no children
                     if(side==0){//if the target is on the left side of parent
                         parent.setLeft(null);
-                    }//if target on left of parent
+                    }
                     else{//if the target is on the right side of parent
                         parent.setRight(null);
-                    }//if target on right
-                }//if target has two children
+                    }
+                }
                 else if(target.getLeft()==null||target.getRight()==null){//if the target has either a right or left child
                     if(side==0){//if target is on the left side of parent
                         if(target.getLeft()==null){//if the target has a right child
                             Student successor=popMax(target.getRight(),parent,0);
                             parent.setLeft(successor);
                             successor.setRight(target.getRight());
-                        }//if target has right child
+                        }
                         else{//if target has a left child
                             Student successor=popMax(target.getLeft(),parent,1);
                             parent.setLeft(successor);
                             successor.setLeft(target.getLeft());
-                        }//if target has left child
-                    }//if target on left of parent
+                        }
+                    }
                     else{//if target is on the right side of the parent
                         if(target.getLeft()==null){//if target has a left child
                             Student successor=popMax(target.getRight(),parent,0);
                             parent.setRight(successor);
                             successor.setRight(target.getRight());
-                        }//if target has left child
+                        }
                         else{//if target has a right child
                             Student successor=popMax(target.getLeft(),parent,1);
                             parent.setRight(successor);
                             successor.setLeft(target.getLeft());
-                        }//if target has right child
-                    }//if target is on right side of parent
-                }//if target has a right or left child
+                        }
+                    }
+                }
                 else{//if the target has two children
                     if(side==0){//if the target is on the left side of the parent
                         Student successor=popMax(target.getLeft(),parent,1);
                         parent.setLeft(successor);
                         successor.setRight(target.getRight());
                         successor.setLeft(target.getLeft());
-                    }//if target is on the left of parent
+                    }
                     else{//if the target is on the right side of the parent
                         Student successor=popMax(target.getLeft(),parent,1);
                         parent.setRight(successor);
                         successor.setRight(target.getRight());
                         successor.setLeft(target.getLeft());
-                    }//if target is on the right of parent
-                }//if the target has two children
-            }//if student key<target key
-        }//else 
-    }//delete two
+                    }
+                }
+            }
+        }
+    }
 
 
     //popMax
@@ -478,32 +436,25 @@ public class BinaryTree {
     private Student popMax(Student branch, Student parent, int side){
         if(side==0&&branch.getLeft()!=null){//if going left and there is no left child
             return popMax(branch.getLeft(),branch,0);
-        }//if going left & no left child
+        }
         else if(side==1&&branch.getRight()!=null){//if going right and there is no right child
             return popMax(branch.getRight(),branch,1);
-        }//if going right & no right child
+        }
         else{//if going and there is a child
             if(side==0){//if going left
                 parent.setLeft(null);
                 return branch;
-            }//if going left
+            }
             else{//if going right
                 parent.setRight(null);
                 return branch;
-            }//if going right
-        }//if going and there is a child
+            }
+        }
     }//delete
 
 
     //printTree()
     //prints Students in BST with their left and right Children
-
-    public void printTree() {
-        System.out.println();
-        printTree2(head);
-        System.out.println();
-    }//printTree()
-
     public void printTree(PrintWriter out) {
         printTree2(head, out);
     }
