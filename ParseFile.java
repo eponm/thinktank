0e87eb0508640cb21d939d4194ae91968b0e8222
jessica-spencer <jspence4@conncoll.edu>
2015-05-06 21:09:48 -0400

    ParseFile will take in the file as a string and then create students and ideas from the values in the file

---
+++ ParseFile.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class ParseFile(String file) {
+    //instance variables??
+    String file;
+    //constructor??
+    public ParseFile () {
+        file=file;
+    
+    //methods!
+    public static void main(String[] args) {
+        //whole bunch of conditionals that sort what type of function 
+        //will be called
+        
+        //first you read in everything, then split by '\n'
+        //splits file by newline
+        String[] lineList= file.split("\n");
+        for (int i=0; i<lineList.length; i++) {
+            String line=lineList[i];
+            String[] classString= line.split("=");
+            for (int j=0; j<classString.length; i++) {
+                //now look for the first object in this array
+                // because the lines look like this:
+                // student = karos, jason, 2323, 4343
+                //when it splits it is ['student','karos,jason,2323,4343']
+                
+                //for STUDENTS
+                if (classString[0]=='student') {
+                    String[] params=classString[1].split(",");
+                    //now it looks like ['karos','jason','2323','4343']
+                    //read in here
+                    Student student=new Student(params[0], params[1], Int.parseInt(params[2]), Int.parseInt(params[3]));
+                    
+                }//student if
+                
+                //for IDEAS
+                else if (classString[0]=='idea') {
+                    String[] params=classString[1].split(",");
+                    //now looks like ['ssn','description','rating
+                    //read in here
+                    Idea idea=new Idea(Int.parseInt(params[0]),params[1],Int.parseInt(params[2]));
+                }//idea if
+            
+        }
+        
+        //then for each string there, split by '='
+         
+        //for each of those, search the first string, see what function
+        //it will be
+        
+        //create a series of conditionals that account for each of these
+        //funtions, place parameters in the function
+    }
