906b7b5764836ee2ff5f45b790d61e6bb60aa0b4
jessica-spencer <jspence4@conncoll.edu>
2015-05-09 16:34:18 -0400

    fixing display Ideas in Students

--- Student.java
+++ Student.java
@@ -81,6 +81,9 @@ class Student { // implements Serializable
     //displayIdeas()
     // to display 10 recent ideas
     public void displayIdeas(){
+        if (isEmpty==true) {
+            System.out.println("This student has no ideas");
+        }
         for(int i=0;i<10;i++){
             Idea[] ideas = ideaQueue.getAll();
             Idea x = ideas[i];
