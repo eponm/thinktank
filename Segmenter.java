36db1bf8ee43697aac7b82e356b8615625a1da78
jskaros <jskaros16@outlook.com>
2015-05-10 12:53:08 -0400

    List has new method getLength and Segmenter has beeen coded

---
+++ Segmenter.java
@@ -0,0 +1,35 @@
+class Segmenter{
+	private int listLength;
+	private int treeSize;
+
+	//Constructor 
+	public Segmenter(){
+		listLength=0;
+		treeSize=0
+	}
+
+	//*************getMethods************
+
+	//Returns listLength int
+	public int getListLength(){
+		return listLength;
+	}//getListLength
+
+
+	//Returns tree size int
+	public int getTreeSize(){
+		return treeSize;
+	}//get List Length
+
+	//**************setMethods************
+
+	//Sets listLength to given int
+	public void setListLength(int newSize){
+		listLength=newSize;
+	}//setListLength
+
+	//Sets treeSize to given int
+	public void setTreeSize(int newSize){
+		treeSize=newSize;
+	}//setTreeSize
+}
