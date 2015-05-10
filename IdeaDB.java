36f0bed189c591ca0d9ecd513fd117c7a01ec022
jessica-spencer <jspence4@conncoll.edu>
2015-05-10 12:50:51 -0400

    chagning size to getTreeSize()

--- IdeaDB.java
+++ IdeaDB.java
@@ -51,7 +51,7 @@ class IdeaDB implements Serializable {
 
     //returns size of SSN tree
     public int getTreeSize() {
-        return studentSSNTree.size();
+        return studentSSNTree.getTreeSize();
     }
 
     //returns size of core list of ideas
