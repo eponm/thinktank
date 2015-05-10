d6831b412c781c1d7c75e169ca4a43dc2291e344
jessica-spencer <jspence4@conncoll.edu>
2015-05-10 12:03:41 -0400

    Queue up to standard

--- Queue.java
+++ Queue.java
@@ -9,27 +9,31 @@ class Queue implements Serializable { //implements Serializable
     private int n;
     private Idea[] queue=new Idea[10];
 
+    //constructor :
+    //set size to 10, tracks front end and length of list 
     public Queue() {
         size=10;
         front=0;
         end=0;
         n=0;
-        //Idea[] queue = new Idea[10];
-    }
-
+    }//constructor
+    
+    //Enqueue() -- puts student into queue, knocks out 1st idea if 11th idea comes in
+    //Params: Idea
      public void enqueue(Idea x) {
         if (n==10) {
             front=(front+1)%10;
             queue[end]=x;
             n=10;
-        }
+        }//if
         else{
             queue[end]=x;
             n++;
-         }
+         }//else
         end=(end+1) % 10;
-    }
+    }//enqueue
 
+    //printQueue() prints to screen
     public void printQueue() {
         //System.out.println(front);
         //System.out.println(end);
@@ -41,38 +45,44 @@ class Queue implements Serializable { //implements Serializable
                System.out.println(queue[i].getKey());
            for(int i = 0; i < end; i++)
                System.out.println(queue[i].getKey());
-        }
-    }
+        }//else
+    }//printQueue()
+    
+    //isEmpty() checks if queue is empty
     public boolean isEmpty() {
         if (front==end) {
             return true;
-        }
+        }//if
         else {
             return false;
-        }
-    }
+        }//else
+    }//isEmpty
 
+    //front() returns first item in queue
     public Idea front() {
         if (isEmpty() ) {
             return null;
-        }
+        }//if
         else {
             return queue[front];
-        }
-    }
+        }//else
+    }//front()
 
+    //dequeue() removes first item from front of queue
     public Idea dequeue() {
         Idea temp=queue[front];
         front=(front+1)% 10;
         return temp;
-    }
+    }//dequeue
 
+    //getAll() returns the entirety of the student idea queue
     public Idea[] getAll() {
         return queue;
-    }
+    }//getAll()
 
+    //getSize returns the size of the queue
     public int getSize() {
         return n;
-    }
+    }//getSize
 
 } // class
